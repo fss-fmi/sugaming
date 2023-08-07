e23bb1ecf475312052d179317896ccdc90063549
Angel Penchev <angel.penchev.tsb@gmail.com>
2023-08-07 17:26:37 +0300

    [SUG-3] Vercel deployment and Terraform provisioning (#4)
    * [Deploy] Terraform init + Vercel resources setup
    
    Added infrastructure configuration for Vercel.
    Added vercel.json to sugaming-api to specify deployment.
    
    * [Temp] Temporarily removing API rerouting
    
    This is done in effort to test the API vercel deployment.
    
    * [Fix] API vercel.json configuration change
    
    * [Fix] API vercel.json configuration change
    
    * [Fix] API vercel.json rewrite fix
    
    * [Refactor] API vercel.json moved
    
    * [Refactor] API vercel.json moved
    
    * [Fix] URL rewrite rules
    
    * [Fix] Moved vercel.json as it was breaking FE apps
    
    * [Fix] Modified sugaming-api install command and root directory
    
    This change was required as vercel.json should only be applicable for the API application.

---
+++ terraform/sugamin-admin.tf
@@ -0,0 +1,12 @@
+resource "vercel_project" "sugaming-admin" {
+  name      = "sugaming-admin"
+  framework = "nextjs"
+  git_repository = {
+    type = "github"
+    repo = "fss-fmi/sugaming"
+    production_branch = "main"
+  }
+  build_command = "yarn nx build sugaming-admin --prod"
+  output_directory = "dist/apps/sugaming-admin/.next"
+  install_command = "yarn install"
+}
---
+++ terraform/sugamin-site.tf
@@ -0,0 +1,12 @@
+resource "vercel_project" "sugaming-site" {
+  name      = "sugaming-site"
+  framework = "nextjs"
+  git_repository = {
+    type = "github"
+    repo = "fss-fmi/sugaming"
+    production_branch = "main"
+  }
+  build_command = "yarn nx build sugaming-site --prod"
+  output_directory = "dist/apps/sugaming-site/.next"
+  install_command = "yarn install"
+}
