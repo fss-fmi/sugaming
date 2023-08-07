439ca72464ce8f32bfe69e557eb416d73a10e660
Angel Penchev <26301867+angel-penchev@users.noreply.github.com>
2023-08-07 17:20:47 +0300

    [Fix] Modified sugaming-api install command and root directory
    This change was required as vercel.json should only be applicable for the API application.

--- terraform/sugaming-api.tf
+++ terraform/sugaming-api.tf
@@ -7,5 +7,6 @@ resource "vercel_project" "sugaming-api" {
   }
   build_command = "yarn nx build sugaming-api --prod"
   output_directory = "dist/apps/sugaming-api"
-  install_command = "yarn install"
+  install_command = "yarn install --modules-folder=\"../..\""
+  root_directory = "apps/sugaming-api"
 }
