c251c414b0bf45ab3b90d9337bd540478fa041b6
Angel Penchev <26301867+angel-penchev@users.noreply.github.com>
2023-08-15 23:53:57 +0300

    refactor: vercel configurations
    Modified vercel configurations to use proper project roots and nonexistent production branch.

--- terraform/vercel_site.tf
+++ terraform/vercel_site.tf
@@ -5,10 +5,11 @@ resource "vercel_project" "vercel_site" {
   git_repository = {
     type              = "github"
     repo              = "fss-fmi/sugaming"
-    production_branch = "main"
+    production_branch = "release-placeholder"
   }
 
   build_command    = "yarn nx build sugaming-site --prod"
-  output_directory = "dist/apps/sugaming-site/.next"
+  output_directory = "../../dist/apps/sugaming-site/.next"
   install_command  = "yarn install"
+  root_directory   = "apps/sugaming-site"
 }
