d67995a320934d7db29970ea3ed98a53c2c1b6a8
Angel Penchev <26301867+angel-penchev@users.noreply.github.com>
2023-08-17 14:23:38 +0300

    refactor: removed redundant vercel configuration
    Removed sugmaing-api Vercel build config as it was ignored because of builds in vercel.json.

--- terraform/vercel_api.tf
+++ terraform/vercel_api.tf
@@ -7,8 +7,5 @@ resource "vercel_project" "vercel_api" {
     production_branch = "release-placeholder"
   }
 
-  build_command    = "yarn nx build sugaming-api --prod"
-  output_directory = "../../dist/apps/sugaming-api"
-  install_command  = "cd ../.. && yarn install --production"
   root_directory   = "apps/sugaming-api"
 }
