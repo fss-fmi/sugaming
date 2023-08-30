c2fb685f70d1f9c5149c934221a46e08bff18a38
Angel Penchev <26301867+angel-penchev@users.noreply.github.com>
2023-08-30 14:51:41 +0300

    style: convert module to es6 style
    Modified sugaming-api to use ES6 style imports and exports.

--- apps/sugaming-api/src/main.ts
+++ apps/sugaming-api/src/main.ts
@@ -8,11 +8,11 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
-async function bootstrap() {
+export default async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
+  app.setGlobalPrefix(globalPrefix);
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
