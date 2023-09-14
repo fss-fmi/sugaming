949139c6fd9c9de21999abf05fd7a701fa600b7e
Angel Penchev <angel.penchev.tsb@gmail.com>
2023-09-14 19:38:28 +0300

    feat: health check endpoint
    Added an endpoint for verifying the sugaming-api and the database are up and running.

---
+++ apps/sugaming-api/src/health/health.controller.ts
@@ -0,0 +1,27 @@
+import { Controller, Get } from '@nestjs/common';
+import {
+  HealthCheck,
+  HealthCheckResult,
+  HealthCheckService,
+  PrismaHealthIndicator,
+} from '@nestjs/terminus';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Controller('health')
+export class HealthController {
+  constructor(
+    private readonly health: HealthCheckService,
+    private readonly prismaHealth: PrismaHealthIndicator,
+    private readonly prismaService: PrismaService,
+  ) {}
+
+  @Get()
+  @HealthCheck()
+  async check(): Promise<HealthCheckResult> {
+    return this.health.check([
+      () => this.prismaHealth.pingCheck('prisma', this.prismaService),
+    ]);
+  }
+}
+
+export default HealthController;
