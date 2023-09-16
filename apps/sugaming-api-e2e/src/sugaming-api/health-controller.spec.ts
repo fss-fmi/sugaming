e887ba2bfee0810b3d173c7440074c8650824435
Angel Penchev <angel.penchev.tsb@gmail.com>
2023-09-17 02:28:02 +0300

    refactor: rename health endpoint e2e tests
    Renamed other E2E tests for consistency.

--- apps/sugaming-api-e2e/src/sugaming-api/health-controller.spec.ts
+++ apps/sugaming-api-e2e/src/sugaming-api/health-controller.spec.ts
@@ -1,7 +1,7 @@
 import axios, { HttpStatusCode } from 'axios';
 
-describe('GET /api/health', () => {
-  it('should return status OK', async () => {
+describe('HealthController', () => {
+  it('GET /api/health should return status OK', async () => {
     const res = await axios.get('/api/health');
 
     // Verify the API is up
