318f75ad35d34a0c5ff7a2edd065a012a6691e8f
Angel Penchev <angel.penchev.tsb@gmail.com>
2023-11-15 04:54:18 +0200

    feat: error message for refresh tokens
    Added localized error messages for token refreshes.

--- apps/sugaming-api/src/auth/guards/jwt-refresh.guard.ts
+++ apps/sugaming-api/src/auth/guards/jwt-refresh.guard.ts
@@ -6,21 +6,26 @@ import {
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
+import { I18nContext } from 'nestjs-i18n';
+import { appConfig } from '../../app/app.config';
 
 @Injectable()
 export class JwtRefreshGuard implements CanActivate {
   constructor(private jwtService: JwtService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const i18n = I18nContext.current();
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
 
-    if (!token) throw new UnauthorizedException();
+    if (!token) {
+      throw new UnauthorizedException(i18n.t('errors.auth.unauthorized'));
+    }
 
     try {
-      request.user = this.jwtService.verify(token);
+      request.user = this.jwtService.verify(token, appConfig.jwtRefreshToken);
     } catch {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException(i18n.t('errors.auth.unauthorized'));
     }
 
     return true;
