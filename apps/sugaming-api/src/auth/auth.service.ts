581f70d300b7d180c07be0dbce862c2c6af2e90f
Angel Penchev <angel.penchev.tsb@gmail.com>
2023-11-15 04:58:06 +0200

    fix: incomplete token payloads after refresh
    Resolved an issue, where token payloads were incomplete whenever refreshing tokens.

--- apps/sugaming-api/src/auth/auth.service.ts
+++ apps/sugaming-api/src/auth/auth.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { Users } from '@prisma/client';
 import ms from 'ms';
 import { UsersService } from '../users/users.service';
 import { appConfig } from '../app/app.config';
@@ -18,10 +17,16 @@ export class AuthService {
       : null;
   }
 
-  async login(user: Omit<Users, 'passwordHash'>) {
-    const payload = { email: user.email, sub: user.id };
+  async login(userInformation: { email: string }) {
+    const user = await this.usersService.getByEmail(userInformation.email);
+    const payload = {
+      email: user.email,
+      sub: user.id,
+      name: `${user.firstName} ${user.lastName}`,
+    };
+
     return {
-      user: await this.usersService.getByEmail(user.email),
+      user,
       accessToken: this.jwtService.sign(payload),
       refreshToken: this.jwtService.sign(payload, appConfig.jwtRefreshToken),
       expiresIn: new Date().setTime(
