e971d357edf86df2c37b071616714430a67526b4
Angel Penchev <angel.penchev.tsb@gmail.com>
2023-11-18 03:41:05 +0200

    fix: replaced bcrypt with bcryptjs
    This change was made, as bcrypt was causing runtime issues.

--- apps/sugaming-api/src/users/users.service.ts
+++ apps/sugaming-api/src/users/users.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
