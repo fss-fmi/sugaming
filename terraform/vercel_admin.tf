512cab3e0df8c83dbd0c76ad36d3ec6fcdfa83d2
Angel Penchev <angel.penchev.tsb@gmail.com>
2023-11-18 04:13:11 +0200

    fix: prisma migration on vercel build

--- terraform/vercel_admin.tf
+++ terraform/vercel_admin.tf
@@ -8,7 +8,7 @@ resource "vercel_project" "vercel_admin" {
     production_branch = "release-placeholder"
   }
 
-  build_command    = "yarn prisma generate && yarn nx build sugaming-admin --prod"
+  build_command    = "yarn prisma migrate deploy && yarn prisma generate && yarn nx build sugaming-admin --prod"
   output_directory = "../../dist/apps/sugaming-admin/.next"
   install_command  = "yarn install"
   root_directory   = "apps/sugaming-admin"
--- terraform/vercel_api.tf
+++ terraform/vercel_api.tf
@@ -7,7 +7,7 @@ resource "vercel_project" "vercel_api" {
     production_branch = "release-placeholder"
   }
 
-  build_command    = "yarn prisma generate && yarn nx build sugaming-api --prod"
+  build_command    = "yarn prisma migrate deploy && yarn prisma generate && yarn nx build sugaming-api --prod"
   output_directory = "../../dist/apps/sugaming-api"
   install_command  = "yarn install"
   root_directory   = "apps/sugaming-api"
