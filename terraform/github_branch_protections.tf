42568210adb72a011b79665a5e40e8910c615b08
Angel Penchev <angel.penchev.tsb@gmail.com>
2023-11-25 04:05:54 +0200

    ci: ci and build and publish workflows as required
    Made the CI and Build and publish workflows required before merging in the main branch.

--- terraform/github_branch_protections.tf
+++ terraform/github_branch_protections.tf
@@ -8,11 +8,14 @@ resource "github_branch_protection_v3" "main_branch_protection" {
     checks = [
       "Vercel – sugaming-api:8329",
       "Vercel – sugaming-site:8329",
-      "Vercel – sugaming-admin:8329"
+      "Vercel – sugaming-admin:8329",
+      "Continuous Integration:15368",
+      "Build and Publish container images:15368"
     ]
   }
 
   required_pull_request_reviews {
-    dismiss_stale_reviews = false
+    dismiss_stale_reviews      = false
+    require_code_owner_reviews = true
   }
 }
