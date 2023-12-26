4565d87470eecd6de81c86e4693a1faeaecc58f5
Angel Penchev <angel.penchev.tsb@gmail.com>
2023-12-27 01:34:35 +0200

    feat: improve team creation validation
    Added enhanced validation to team creation and updated internationalization (i18n) error messages.
    The checks verify if a team with the same name already exists and
    if the user is already a member of a different team.
    The checks are performed before a team is created to prevent duplicates/incorrect team assignments.

--- apps/sugaming-api/src/cs2/teams/cs2-teams.service.ts
+++ apps/sugaming-api/src/cs2/teams/cs2-teams.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { I18nContext } from 'nestjs-i18n';
 import { CreateTeamDto } from './dto/create-team.dto';
 import { PrismaService } from '../../prisma/prisma.service';
 
@@ -7,6 +8,33 @@ export class Cs2TeamsService {
   constructor(private readonly prisma: PrismaService) {}
 
   async create(createTeamDto: CreateTeamDto, capitanId: string) {
+    const i18n = I18nContext.current();
+
+    // Check if team name is already taken
+    const teamNameExists = await this.prisma.cs2Teams.findFirst({
+      where: {
+        name: createTeamDto.name,
+      },
+    });
+
+    if (teamNameExists) {
+      throw new BadRequestException(
+        i18n.t('errors.cs2.teams.nameAlreadyExists'),
+      );
+    }
+
+    // Check if user is already in a team
+    const teamsCaptainIsPartOf = await this.prisma.cs2Teams.findMany({
+      where: {
+        OR: [{ capitanId }, { members: { some: { id: capitanId } } }],
+      },
+    });
+
+    if (teamsCaptainIsPartOf.length > 0) {
+      throw new BadRequestException(i18n.t('errors.cs2.teams.alreadyInTeam'));
+    }
+
+    // Create the team
     return this.prisma.cs2Teams.create({
       data: {
         name: createTeamDto.name,
