0b386804a68c5d092ed622bf25497858e15f64cd
Angel Penchev <angel.penchev.tsb@gmail.com>
2023-12-27 02:41:32 +0200

    test: team creation in cs2 teams service
    Updated Prisma service mock and user mocks to accommodate new Cs2Teams table.
    Tests for Cs2TeamsService now cover scenarios of
    valid team creation, team name duplication and user already being in a team.

--- apps/sugaming-api/src/users/users.mock.ts
+++ apps/sugaming-api/src/users/users.mock.ts
@@ -4,6 +4,7 @@ export const exampleUserWithoutPassword = {
   firstName: 'Gosho',
   lastName: 'Losho',
   nickname: 'Reomak',
+  cs2TeamId: 1,
   createdAt: new Date(),
   updatedAt: new Date(),
 };
@@ -22,10 +23,10 @@ export const exampleUser = {
 export const exampleUserWithoutPassword2 = {
   id: 'eb11f360-98c5-44f3-9b4a-98169b051077',
   email: 'posho.minava.moshava@gmail.com',
-  passwordHash: 'hashedPassword',
   firstName: 'Posho',
   lastName: 'Mosho',
   nickname: 'poshto moshavam',
+  cs2TeamId: 2,
   createdAt: new Date(),
   updatedAt: new Date(),
 };
@@ -38,12 +39,29 @@ export const exampleUserCredentials2 = {
 export const exampleUser2 = {
   // Hashed and salted version of "GoshoLoshoTestPassword"
   passwordHash: '$2b$10$ieK.GfGsIiCJXdmsxbTmzutpPmekEQQsy9xhoS6iNggqv/q68IgKq',
-  ...exampleUserWithoutPassword,
+  ...exampleUserWithoutPassword2,
 };
 
-export const exampleUsers = [exampleUser, exampleUser2];
+export const exampleUserWithoutPassword3 = {
+  id: '35f8f475-f008-4c5e-88f3-5e453b6904b7',
+  email: 'posho.mosho@gmail.com',
+  firstName: 'Posho',
+  lastName: 'Mosho',
+  nickname: 'poshomosho',
+  cs2TeamId: null,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
 
-export const exampleUsersWithoutPasswords = [
-  exampleUserWithoutPassword,
-  exampleUserWithoutPassword2,
-];
+export const exampleUserCredentials3 = {
+  email: 'posho.mosho@gmail.com',
+  password: 'GoshoLoshoTestPassword',
+};
+
+export const exampleUser3 = {
+  // Hashed and salted version of "GoshoLoshoTestPassword"
+  passwordHash: '$2b$10$ieK.GfGsIiCJXdmsxbTmzutpPmekEQQsy9xhoS6iNggqv/q68IgKq',
+  ...exampleUserWithoutPassword3,
+};
+
+export const exampleUsers = [exampleUser, exampleUser2, exampleUser3];
