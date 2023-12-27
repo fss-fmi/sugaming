e2803a3cf0cb6b654a71ccf20d0a9b36a4f6b2c3
Angel Penchev <angel.penchev.tsb@gmail.com>
2023-12-28 01:21:08 +0200

    fix: revise api version in endpoint tests
    Updated the start API check in sugaming-api-e2e, sugaming-admin-e2e and sugaming-site-e2e projects.
    The old version was 'GET /api/health', which has now been updated to 'GET /api/v1/health'.

--- apps/sugaming-api-e2e/src/sugaming-api/health-controller.spec.ts
+++ apps/sugaming-api-e2e/src/sugaming-api/health-controller.spec.ts
@@ -1,7 +1,7 @@
 import axios, { HttpStatusCode } from 'axios';
 
 describe('HealthController', () => {
-  it('GET /api/health should return status OK', async () => {
+  it('GET /api/v1/health should return status OK', async () => {
     const res = await axios.get('/api/v1/health');
 
     // Verify the API is up
