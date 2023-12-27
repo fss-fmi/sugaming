9559c5b398619d33b52037cf78ba4b95fb0e9d0b
Angel Penchev <angel.penchev.tsb@gmail.com>
2023-12-28 00:29:51 +0200

    fix: fix endpoint in health controller api e2e test
    The commit fixes the endpoint used in the 'GET /api/health' test within the HealthController.
    The previous endpoint was '/api/health' and it has been updated to '/api/v1/health'.

--- apps/sugaming-api-e2e/src/sugaming-api/health-controller.spec.ts
+++ apps/sugaming-api-e2e/src/sugaming-api/health-controller.spec.ts
@@ -2,7 +2,7 @@ import axios, { HttpStatusCode } from 'axios';
 
 describe('HealthController', () => {
   it('GET /api/health should return status OK', async () => {
-    const res = await axios.get('/api/health');
+    const res = await axios.get('/api/v1/health');
 
     // Verify the API is up
     expect(res.status).toBe(HttpStatusCode.Ok);
