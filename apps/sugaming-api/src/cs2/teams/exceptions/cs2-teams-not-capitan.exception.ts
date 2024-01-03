0305e83aa03e9286d55c25192924e27f6b74ff05
Angel Penchev <angel.penchev.tsb@gmail.com>
2024-01-03 02:05:46 +0200

    fix: typo in 'notCaptain' exception message
    Fixed a spelling error in the 'notCaptain' error message key in cs2-teams-not-capitan.exception.
    Simultaneously, added missing respective translations for the error message.

--- apps/sugaming-api/src/cs2/teams/exceptions/cs2-teams-not-capitan.exception.ts
+++ apps/sugaming-api/src/cs2/teams/exceptions/cs2-teams-not-capitan.exception.ts
@@ -4,6 +4,6 @@ import { I18nContext } from 'nestjs-i18n';
 export class Cs2TeamsNotCapitanException extends HttpException {
   constructor() {
     const i18n = I18nContext.current();
-    super(i18n.t('errors.cs2.teams.notCapitan'), HttpStatus.FORBIDDEN);
+    super(i18n.t('errors.cs2.teams.notCaptain'), HttpStatus.FORBIDDEN);
   }
 }
