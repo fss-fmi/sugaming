c7789d416d88d819cb952348c92d9e964ed5f550
Angel Penchev <angel.penchev.tsb@gmail.com>
2024-01-03 03:03:13 +0200

    refactor: rename 'cs2_teams_invitations' to 'cs2_team_invitations'
    The 'cs2_teams_invitations' table and its references have been renamed to 'cs2_team_invitations'.
    This was done in order to correctly reflect the model in Prisma schema.
    This name change has been reflected in both the schema and the migration file.

--- prisma/migrations/20231228011157_cs2_team_invitations_and_requests/migration.sql
+++ prisma/migrations/20231228011157_cs2_team_invitations_and_requests/migration.sql
@@ -2,13 +2,13 @@
 ALTER TABLE "Cs2Teams" DROP CONSTRAINT "Cs2Teams_capitan_id_fkey";
 
 -- CreateTable
-CREATE TABLE "cs2_teams_invitations" (
+CREATE TABLE "cs2_team_invitations" (
     "id" SERIAL NOT NULL,
     "cs2_team_id" INTEGER NOT NULL,
     "user_id" TEXT NOT NULL,
     "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT "cs2_teams_invitations_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "cs2_team_invitations_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -25,10 +25,10 @@ CREATE TABLE "cs2_team_requests" (
 ALTER TABLE "Cs2Teams" ADD CONSTRAINT "Cs2Teams_capitan_id_fkey" FOREIGN KEY ("capitan_id") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "cs2_teams_invitations" ADD CONSTRAINT "cs2_teams_invitations_cs2_team_id_fkey" FOREIGN KEY ("cs2_team_id") REFERENCES "Cs2Teams"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "cs2_team_invitations" ADD CONSTRAINT "cs2_team_invitations_cs2_team_id_fkey" FOREIGN KEY ("cs2_team_id") REFERENCES "Cs2Teams"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "cs2_teams_invitations" ADD CONSTRAINT "cs2_teams_invitations_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "Users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "cs2_team_invitations" ADD CONSTRAINT "cs2_team_invitations_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "Users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "cs2_team_requests" ADD CONSTRAINT "cs2_team_requests_cs2_team_id_fkey" FOREIGN KEY ("cs2_team_id") REFERENCES "Cs2Teams"("id") ON DELETE CASCADE ON UPDATE CASCADE;
