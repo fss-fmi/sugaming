8545106daede8395467643aa7a514f45f790d796
Angel Penchev <angel.penchev.tsb@gmail.com>
2024-01-06 03:37:40 +0200

    fix: rename keys in prisma service mock
    The keys 'users' and 'cs2Teams' in prismaServiceMock have been renamed to 'user' and 'cs2Team'.
    This change was required due to the recent prisma model name changes.

--- apps/sugaming-api/src/prisma/prisma.service.mock.ts
+++ apps/sugaming-api/src/prisma/prisma.service.mock.ts
@@ -3,8 +3,8 @@ import { exampleUsers } from '../users/users.mock';
 import { exampleCs2Teams } from '../cs2/teams/cs2-teams.mock';
 
 const prismaServiceMock = createPrismaMock({
-  users: exampleUsers,
-  cs2Teams: exampleCs2Teams,
+  user: exampleUsers,
+  cs2Team: exampleCs2Teams,
 });
 
 export default prismaServiceMock;
