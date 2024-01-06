b5737568e2d6a6b55efdcf395436130ea0e127de
Angel Penchev <angel.penchev.tsb@gmail.com>
2024-01-06 02:15:23 +0200

    refactor: database schema naming conventions
    Applied schema naming conventions into alignment by changing all schema designations to lowercase.
    This includes table names, column names, indices, constraints, and model names.

--- prisma/migrations/20231226010640_cs2_teams_init/migration.sql
+++ prisma/migrations/20231226010640_cs2_teams_init/migration.sql
@@ -1,29 +1,30 @@
 -- CreateEnum
-CREATE TYPE "Color" AS ENUM ('RED', 'ORANGE', 'YELLOW', 'GREEN', 'BLUE', 'PURPLE', 'PINK', 'BROWN', 'NAVY', 'VIOLET', 'CYAN', 'MAGENTA', 'LIME', 'TEAL', 'INDIGO', 'CORAL');
+CREATE TYPE "color" AS ENUM ('RED', 'ORANGE', 'YELLOW', 'GREEN', 'BLUE', 'PURPLE', 'PINK', 'BROWN', 'NAVY', 'VIOLET', 'CYAN', 'MAGENTA', 'LIME', 'TEAL', 'INDIGO', 'CORAL');
 
 -- AlterTable
-ALTER TABLE "Users" ADD COLUMN     "cs2_team_id" INTEGER;
+ALTER TABLE "users" ADD COLUMN "cs2_team_id" INTEGER;
 
 -- CreateTable
-CREATE TABLE "Cs2Teams" (
+CREATE TABLE "cs2_teams" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
-    "color" "Color" NOT NULL,
+    "color" "color" NOT NULL,
     "capitan_id" TEXT,
     "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMPTZ NOT NULL,
 
-    CONSTRAINT "Cs2Teams_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "cs2_teams_pkey" PRIMARY KEY ("id")
 );
 
--- CreateIndex
-CREATE UNIQUE INDEX "Cs2Teams_name_key" ON "Cs2Teams"("name");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Cs2Teams_capitan_id_key" ON "Cs2Teams"("capitan_id");
+CREATE UNIQUE INDEX "cs2_teams_name_key" ON "cs2_teams"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "cs2_teams_capitan_id_key" ON "cs2_teams"("capitan_id");
 
 -- AddForeignKey
-ALTER TABLE "Users" ADD CONSTRAINT "Users_cs2_team_id_fkey" FOREIGN KEY ("cs2_team_id") REFERENCES "Cs2Teams"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "users" ADD CONSTRAINT "users_cs2_team_id_fkey" FOREIGN KEY ("cs2_team_id") REFERENCES "cs2_teams"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Cs2Teams" ADD CONSTRAINT "Cs2Teams_capitan_id_fkey" FOREIGN KEY ("capitan_id") REFERENCES "Users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "cs2_teams" ADD CONSTRAINT "cs2_teams_capitan_id_fkey" FOREIGN KEY ("capitan_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
