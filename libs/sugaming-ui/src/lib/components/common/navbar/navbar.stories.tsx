d36d28d15ab4db69543428590d14bca8d8cce3a5
Angel Penchev <angel.penchev.tsb@gmail.com>
2024-01-15 00:47:48 +0200

    docs: add storybook files for navbar and badge components
    Two new files have been added for Storybook UI documentation.
    These are specific to the 'Navbar' and 'Badge' components of the 'sugaming-ui' library.

---
+++ libs/sugaming-ui/src/lib/components/common/navbar/navbar.stories.tsx
@@ -0,0 +1,44 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { NavbarLinks } from '../../site/navbar-links/navbar-links';
+import { NavbarUserControls } from '../../site/navbar-user-controls/navbar-user-controls';
+import { Navbar } from './navbar';
+
+const meta: Meta<typeof Navbar> = {
+  title: 'Common/Navbar',
+  component: Navbar,
+  argTypes: {
+    children: {
+      type: 'string',
+      defaultValue: 'Navbar',
+      description: 'Navbar content',
+      table: {
+        type: {
+          summary: 'string',
+        },
+      },
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Navbar>;
+
+export const DefaultNavbar: Story = {
+  args: {
+    children: (
+      <>
+        <NavbarLinks className="block xl:hidden" variant="mobile" />
+
+        <span className="font-bold capitalize">SUGAMING</span>
+
+        <NavbarLinks
+          className="hidden xl:flex justify-center"
+          variant="desktop"
+        />
+
+        <NavbarUserControls user={undefined} className="" />
+      </>
+    ),
+  },
+};
