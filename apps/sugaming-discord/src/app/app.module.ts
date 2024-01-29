abaec6dfeeaa16544f0c5915fa0990da28ace1eb
Angel Penchev <angel.penchev.tsb@gmail.com>
2024-01-29 03:56:36 +0200

    feat: add i18n support and update ping command
    Added internationalization support in the application based on the guild preferred language.
    Refactored the ping bot command to respond with a localized ping-pong message.

--- apps/sugaming-discord/src/app/app.module.ts
+++ apps/sugaming-discord/src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { Module } from '@nestjs/common';
 
 import { DiscordModule } from '@discord-nestjs/core';
+import { I18nModule } from 'nestjs-i18n';
+import path from 'path';
 import { appConfig } from './app.config';
 import { BotModule } from '../bot/bot.module';
 import { ManagementModule } from '../management/management.module';
+import { DiscordI18nResolver } from '../resolvers/discord-i18n.resolver';
 
 @Module({
   imports: [
     DiscordModule.forRootAsync({
       useFactory: () => appConfig.discord,
+    }),
+    I18nModule.forRoot({
+      fallbackLanguage: 'bg',
+      loaderOptions: {
+        path: path.join(__dirname, 'assets', 'i18n'),
+        watch: true,
+      },
+      resolvers: [DiscordI18nResolver],
     }),
     BotModule,
     ManagementModule,
--- apps/sugaming-discord/src/management/commands/ping.command.ts
+++ apps/sugaming-discord/src/management/commands/ping.command.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { Command, Handler } from '@discord-nestjs/core';
 import { CommandInteraction } from 'discord.js';
+import { I18nContext } from 'nestjs-i18n';
 import { EnableMethodLoggerDecorator } from '../../decorators/enable-method-logger.decorator';
 
 @Injectable()
 @Command({
   name: 'ping',
-  description: 'Изпраща информация за всички събития по време на хакатона',
+  description: 'Отговаря с "Pong!" когато ботът функционира правилно.',
 })
 export class PingCommand {
   @Handler()
   @EnableMethodLoggerDecorator()
-  async handler(interaction: CommandInteraction): Promise<string> {
-    return `🏓 Pong! ${Math.round(
+  async pingCommandHandler(interaction: CommandInteraction) {
+    const i18n = I18nContext.current();
+
+    return `🏓 ${i18n.t('management.messages.pong')} ${Math.round(
       Date.now() - interaction.createdTimestamp,
     )}ms`;
   }
