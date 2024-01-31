649150d716215b7def7bfa237a1640fecf9dc0f4
Angel Penchev <angel.penchev.tsb@gmail.com>
2024-01-31 14:19:51 +0200

    feat: login page template
    Updated the login page with a new design including a banner placeholder and terms agreement.
    Adjusted the body height property.

--- apps/sugaming-site/app/[locale]/login/page.tsx
+++ apps/sugaming-site/app/[locale]/login/page.tsx
@@ -1,6 +1,8 @@
 import { LoginForm } from '@sugaming/sugaming-ui/lib/components/site/login-form/login-form';
 import { getUser } from '@sugaming/sugaming-api-client/next';
 import { redirect, RedirectType } from 'next/navigation';
+import Link from 'next/link';
+import { Card } from '@sugaming/sugaming-ui/lib/components/common/server';
 
 export default async function LoginPage() {
   if (await getUser()) {
@@ -8,9 +10,54 @@ export default async function LoginPage() {
   }
 
   return (
-    <>
-      <h1>Log In Page</h1>
-      <LoginForm />
-    </>
+    <Card className="mx-auto mt-10 hidden md:grid h-[750px] w-2/3 flex-col items-center justify-center lg:max-w-24 lg:grid-cols-2 lg:px-0 overflow-hidden">
+      <div className="hidden lg:flex h-full flex-col p-10 text-white bg-zinc-900">
+        <div className="relative z-20 flex items-center text-lg font-medium">
+          Acme Inc
+        </div>
+
+        <div className="relative z-20 mt-auto">
+          <blockquote className="space-y-2">
+            <p className="text-lg">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            </p>
+            <footer className="text-sm">SUGAMING</footer>
+          </blockquote>
+        </div>
+      </div>
+
+      <div className="lg:p-8">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="flex flex-col space-y-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Create an account
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Enter your email below to create your account
+            </p>
+          </div>
+
+          <LoginForm />
+
+          <p className="px-8 text-center text-sm text-muted-foreground">
+            By clicking continue, you agree to our{' '}
+            <Link
+              href="/"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Terms of Service
+            </Link>{' '}
+            and{' '}
+            <Link
+              href="/"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Privacy Policy
+            </Link>
+            .
+          </p>
+        </div>
+      </div>
+    </Card>
   );
 }
--- libs/sugaming-ui/src/lib/components/site/login-form/login-form.tsx
+++ libs/sugaming-ui/src/lib/components/site/login-form/login-form.tsx
@@ -6,6 +6,7 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { login } from '@sugaming/sugaming-api-client/next';
 import { useTranslations } from 'next-intl';
+import { useState } from 'react';
 import { Button } from '../../common/server';
 import {
   Form,
@@ -50,6 +51,8 @@ export function LoginForm() {
     }
   }
 
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   return (
     <>
       <Toaster />
@@ -84,9 +87,30 @@ export function LoginForm() {
             )}
           />
 
-          <Button type="submit">{t('submit')}</Button>
+          <Button type="submit" className="w-full">
+            {t('submit')}
+          </Button>
         </form>
       </Form>
+
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t" />
+        </div>
+        <div className="relative flex justify-center text-xs uppercase">
+          <span className="bg-background px-2 text-muted-foreground">
+            Or continue with
+          </span>
+        </div>
+      </div>
+      <Button variant="outline" type="button" disabled={isLoading}>
+        {isLoading
+          ? // <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+            'gosho'
+          : // <Icons.gitHub className="mr-2 h-4 w-4" />
+            'github'}{' '}
+        GitHub
+      </Button>
     </>
   );
 }
