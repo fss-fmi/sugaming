0e98713c63522fa41049489e7a6817da3f178e40
Angel Penchev <angel.penchev.tsb@gmail.com>
2024-02-16 01:30:06 +0000

    feat: add github organization secrets for sugaming api servers

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -28,12 +28,6 @@ variable "vercel_org_id" {
   sensitive   = false
 }
 
-variable "vercel_project_api_id" {
-  description = "Vercel sugaming-api project ID"
-  type        = string
-  sensitive   = false
-}
-
 variable "vercel_project_site_id" {
   description = "Vercel sugaming-site project ID"
   type        = string
@@ -48,6 +42,66 @@ variable "vercel_project_admin_id" {
 
 variable "fss_fmi_github_token" {
   description = "GitHub API token for the fss-fmi-admin account"
+  type        = string
+  sensitive   = true
+}
+
+variable "sugaming_api_production_public_ip" {
+  description = "Public IP address of the sugaming-api production server"
+  type        = string
+  sensitive   = false
+}
+
+variable "sugaming_api_preview_public_ip" {
+  description = "Public IP address of the sugaming-api preview server"
+  type        = string
+  sensitive   = false
+}
+
+variable "sugaming_api_production_ssh_user" {
+  description = "SSH user for the sugaming-api production server"
+  type        = string
+  sensitive   = false
+}
+
+variable "sugaming_api_preview_ssh_user" {
+  description = "SSH user for the sugaming-api preview server"
+  type        = string
+  sensitive   = false
+}
+
+variable "sugaming_api_production_ssh_password" {
+  description = "SSH password for the sugaming-api production server"
+  type        = string
+  sensitive   = true
+}
+
+variable "sugaming_api_preview_ssh_password" {
+  description = "SSH password for the sugaming-api preview server"
+  type        = string
+  sensitive   = true
+}
+
+variable "sugaming_api_production_nginx_proxy_manager_email" {
+  description = "Email address for the Nginx Proxy Manager admin user"
+  type        = string
+  sensitive   = false
+}
+
+variable "sugaming_api_preview_nginx_proxy_manager_email" {
+  description = "Email address for the Nginx Proxy Manager admin user"
+  type        = string
+  sensitive   = false
+}
+
+variable "sugaming_api_production_nginx_proxy_manager_password" {
+  description = "Password for the Nginx Proxy Manager admin user"
+  type        = string
+  sensitive   = true
+}
+
+variable "sugaming_api_preview_nginx_proxy_manager_password" {
+  description = "Password for the Nginx Proxy Manager admin user"
   type        = string
   sensitive   = true
 }
