cc4eccdfe0a8559418fd03d1b39cbf49951d38f6
Angel Penchev <angel.penchev.tsb@gmail.com>
2024-02-24 02:38:48 +0200

    ci: update vercel and github provider versions in terraform
    Gosdijfosdjf sdjf osdijf osdijf osidjf oisdjf oisdjf12345678901234567890
    The Vercel provider version in the Terraform configuration has been
    updated from version 0.3 to version 1.1.0.
    Similarly, the Github provider version has been updated from version 5.0
    to version 6.0.0.
    This update ensures that the Terraform configuration is working with the
    latest stable versions of these providers.

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     vercel = {
       source  = "vercel/vercel"
-      version = "~> 0.3"
+      version = "1.1.0"
     }
 
     github = {
       source  = "integrations/github"
-      version = "~> 5.0"
+      version = "6.0.0"
     }
   }
 
@@ -21,9 +21,11 @@ terraform {
 }
 
 provider "vercel" {
+  # Optional: Set your Vercel team if you want to deploy to a specific team
   team = "fss-fmi"
 }
 
 provider "github" {
+  # Optional: Set your GitHub owner if you want to interact with repositories under a specific organization
   owner = "fss-fmi"
 }
