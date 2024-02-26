088b6f5fc51af34fbaa15cf339177aadb57c5da5
Angel Penchev <angel.penchev.tsb@gmail.com>
2024-02-26 20:07:05 +0200

    feat: add link wrapper to team card component
    The TeamCard component is now wrapped with a Link component to facilitate navigation.
    This modification ensures that when a user clicks on a team card, they are appropriately redirected
    to the respective team page using the team's id in the route.

--- libs/sugaming-ui/src/lib/components/site/team-card/team-card.tsx
+++ libs/sugaming-ui/src/lib/components/site/team-card/team-card.tsx
@@ -2,38 +2,41 @@ import { ApiClient } from '@sugaming/sugaming-api-client/client';
 import libConfig from '@sugaming/sugaming-services/config/lib.config';
 import React from 'react';
 import { SiCounterstrike } from 'react-icons/si';
+import Link from 'next/link';
 import { Logo } from '../logo/logo';
 import { TeamMemberAvatar } from '../team-member-avatar/team-member-avatar';
 import { Card, CardContent, CardHeader } from '../../common/server';
 
 export function TeamCard({ team }: { team: ApiClient.Cs2TeamResponseBodyDto }) {
   return (
-    <Card className="w-full transition hover:-translate-y-1 hover:shadow-lg cursor-pointer">
-      <CardHeader className="relative p-0 w-full aspect-[21/9] space-y-0 overflow-hidden rounded-t-xl">
-        <div className="absolute inset-0 flex flex-col w-full h-full pt-6 bg-blue-900 overflow-ellipsis text-center text-white">
-          <p className="text-3xl font-bold">{team.name.toUpperCase()}</p>
-          <Logo className="mx-auto scale-50 -mt-1" />
-        </div>
-        <div
-          className="absolute inset-0 w-full h-full grid justify-center items-end"
-          style={{
-            padding: `0px ${(libConfig.cs2Team.members.max - team.members.length) * 10}%`,
-            gridTemplateColumns: `repeat(${team.members.length}, 1fr)`,
-          }}
-        >
-          {team.members.map((member) => (
-            <TeamMemberAvatar member={member} key={member.id} />
-          ))}
-        </div>
-      </CardHeader>
-      <CardContent className="p-3">
-        <div className="flex items-center">
-          <SiCounterstrike className="w-6 h-6 mr-2" />
-          <h3 className="text-lg font-bold text-ellipsis overflow-hidden">
-            {team.name}
-          </h3>
-        </div>
-      </CardContent>
-    </Card>
+    <Link href={`/cs2-teams/${team.id}`}>
+      <Card className="w-full transition hover:-translate-y-1 hover:shadow-lg">
+        <CardHeader className="relative p-0 w-full aspect-[21/9] space-y-0 overflow-hidden rounded-t-xl">
+          <div className="absolute inset-0 flex flex-col w-full h-full pt-6 bg-blue-900 overflow-ellipsis text-center text-white">
+            <p className="text-3xl font-bold">{team.name.toUpperCase()}</p>
+            <Logo className="mx-auto scale-50 -mt-1" />
+          </div>
+          <div
+            className="absolute inset-0 w-full h-full grid justify-center items-end"
+            style={{
+              padding: `0px ${(libConfig.cs2Team.members.max - team.members.length) * 10}%`,
+              gridTemplateColumns: `repeat(${team.members.length}, 1fr)`,
+            }}
+          >
+            {team.members.map((member) => (
+              <TeamMemberAvatar member={member} key={member.id} />
+            ))}
+          </div>
+        </CardHeader>
+        <CardContent className="p-3">
+          <div className="flex items-center">
+            <SiCounterstrike className="w-6 h-6 mr-2" />
+            <h3 className="text-lg font-bold text-ellipsis overflow-hidden">
+              {team.name}
+            </h3>
+          </div>
+        </CardContent>
+      </Card>
+    </Link>
   );
 }
