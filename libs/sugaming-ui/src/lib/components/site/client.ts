c2089e9eeac010f95b47e439a4ec318cbc155425
Angel Penchev <angel.penchev.tsb@gmail.com>
2024-02-26 17:35:45 +0200

    refactor: add new team member avatar component
    Introduced a new component TeamMemberAvatar which extracts the avatar functionality.
    This enhances code modularity and reusability and allows team card to be rendered on the server.
    Changes also propagated to team-card.tsx where the component was initially used.

--- libs/sugaming-ui/src/lib/components/site/client.ts
+++ libs/sugaming-ui/src/lib/components/site/client.ts
@@ -2,3 +2,4 @@ export * from './login-form/login-form';
 export * from './navbar-links/navbar-links';
 export * from './navbar-user-controls/navbar-user-controls';
 export * from './sign-up-form/sign-up-form';
+export * from './team-member-avatar/team-member-avatar';
