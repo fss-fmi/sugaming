f2666a553eb254fdb777e1dec31e16d867d21a9c
Angel Penchev <angel.penchev.tsb@gmail.com>
2024-02-28 05:27:54 +0200

    feat: update teams page with enhanced user queries
    Added multiple new queries to improve user filtering on the teams page.
    The new queries include ways to get users without a team and no requests or invites, users with a
    team and no requests or invites, users who requested to join, and users who are already invited.
    Updated PlayerSearch component with these new queries.

--- apps/sugaming-site/app/[locale]/cs2/teams/[id]/page.tsx
+++ apps/sugaming-site/app/[locale]/cs2/teams/[id]/page.tsx
@@ -13,16 +13,72 @@ import { useLocale } from 'next-intl';
 import { PlayerSearch } from '@sugaming/sugaming-ui/lib/components/site/client';
 import React, { Suspense } from 'react';
 import { SiCounterstrike } from 'react-icons/si';
+import { getBearerToken } from '@sugaming/sugaming-api-client/next';
 
 export default async function CS2TeamPage() {
-  const t = await getTranslations('cs2-teams-page');
+  const t = await getTranslations('cs2-team-page');
   const locale = useLocale();
   const teams = await ApiClient.Cs2TeamsApiService.cs2TeamsControllerGetV1({});
   const team = teams[0]; // TODO: Refactor to get team by id
 
+  // TODO: this is requested for each subcategory, refactor to get all users once
   async function getUsersOutsideOfThisTeam() {
     const users = await ApiClient.UsersApiService.usersControllerGetAllV1({});
     return users.filter((user) => user.cs2Team?.id !== team.id);
+  }
+
+  async function getTeamInvitesSentUserIds() {
+    const invites =
+      await ApiClient.Cs2TeamsApiService.cs2TeamsControllerGetInvitationsSentV1(
+        {
+          authorization: await getBearerToken(),
+        },
+      );
+    return invites.map((invite) => invite.userId);
+  }
+
+  async function getTeamJoinRequestsUserIds() {
+    const requests =
+      await ApiClient.Cs2TeamsApiService.cs2TeamsControllerGetJoinRequestsV1({
+        authorization: await getBearerToken(),
+      });
+    return requests.map((request) => request.userId);
+  }
+
+  async function getUsersWithoutATeamAndNoRequestAndInvites() {
+    const users = await getUsersOutsideOfThisTeam();
+    const invitesUserIds = await getTeamInvitesSentUserIds();
+    const requestsUserIds = await getTeamJoinRequestsUserIds();
+
+    return users
+      .filter((user) => !user.cs2Team)
+      .filter((user) => !invitesUserIds.includes(user.id))
+      .filter((user) => !requestsUserIds.includes(user.id));
+  }
+
+  async function getUsersWithATeamAndNoRequestAndInvites() {
+    const users = await ApiClient.UsersApiService.usersControllerGetAllV1({});
+    const invitesUserIds = await getTeamInvitesSentUserIds();
+    const requestsUserIds = await getTeamJoinRequestsUserIds();
+
+    return users
+      .filter((user) => user.cs2Team)
+      .filter((user) => !invitesUserIds.includes(user.id))
+      .filter((user) => !requestsUserIds.includes(user.id));
+  }
+
+  async function getUsersRequestedToJoin() {
+    const users = await getUsersOutsideOfThisTeam();
+    const requestsUserIds = await getTeamJoinRequestsUserIds();
+
+    return users.filter((user) => requestsUserIds.includes(user.id));
+  }
+
+  async function getUsersAlreadyInvited() {
+    const users = await getUsersOutsideOfThisTeam();
+    const invitesUserIds = await getTeamInvitesSentUserIds();
+
+    return users.filter((user) => invitesUserIds.includes(user.id));
   }
 
   return (
@@ -41,7 +97,10 @@ export default async function CS2TeamPage() {
         <Suspense fallback={null}>
           <PlayerSearch
             teamId={team.id}
-            users={await getUsersOutsideOfThisTeam()}
+            usersWithoutTeam={await getUsersWithoutATeamAndNoRequestAndInvites()}
+            usersWithTeam={await getUsersWithATeamAndNoRequestAndInvites()}
+            usersRequestedToJoin={await getUsersRequestedToJoin()}
+            usersAlreadyInvited={await getUsersAlreadyInvited()}
           >
             <Button className="absolute bottom-1 right-1 rounded-xl">
               <FaUserPlus className="h-4 w-4" />
