4aa477a3c87fd986a4990bdf07a5f6f91a23ce8b
Antoniya Hristova <karma12451@gmail.com>
2024-02-29 01:11:08 +0200

    feat: updated avatar

--- libs/sugaming-ui/src/lib/components/site/onboarding-dialog/components/avatar-step.tsx
+++ libs/sugaming-ui/src/lib/components/site/onboarding-dialog/components/avatar-step.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 import { useTranslations } from 'next-intl';
 import React, { useState } from 'react';
 import { avataaars } from '@dicebear/collection';
@@ -8,30 +9,77 @@ import { Button } from '../../../common/server';
 import { DialogFooter } from '../../../common/client';
 
 const options = {
-  mouth: ['default', 'smile', 'sad', 'serious', 'screamOpen'],
+  mouth: ['default', 'smile', 'sad', 'serious', 'screamOpen', 'tongue'],
   top: ['hat', 'turban', 'hijab', 'bigHair', 'bob', 'straight01'],
   accessories: [
-    'none',
+    'eyepatch',
+    'kurt',
     'prescription01',
     'prescription02',
-    'roundGlasses',
+    'round',
     'sunglasses',
+    'wayfarers',
   ],
-  hairColor: ['brown', 'black', 'blonde', 'red'],
+  hairColor: [
+    'f59797',
+    'ecdcbf',
+    'e8e1e1',
+    'd6b370',
+    'c93305',
+    'b58143',
+    'a55728',
+    '724133',
+    '4a312c',
+    '2c1b18',
+  ],
   accessoryColor: ['black', 'blue', 'gray', 'green'],
+  eyes: [
+    'closed',
+    'cry',
+    'default',
+    'eyeRoll',
+    'happy',
+    'hearts',
+    'side',
+    'squint',
+    'surprised',
+    'wink',
+    'winkWacky',
+    'xDizzy',
+  ],
+  eyebrows: [
+    'angry',
+    'angryNatural',
+    'default',
+    'defaultNatural',
+    'flatNatural',
+    'frownNatural',
+    'raisedExcited',
+    'raisedExcitedNatural',
+    'sadConcerned',
+    'sadConcernedNatural',
+    'unibrowNatural',
+    'upDown',
+    'upDownNatural',
+  ],
+  facialHair: [
+    'beardLight',
+    'beardMajestic',
+    'beardMedium',
+    'moustacheFancy',
+    'moustacheMagnum',
+  ],
 };
 
 interface CustomizationOptions {
-  mouth: 'default' | 'smile' | 'sad' | 'serious' | 'screamOpen';
-  top: 'hat' | 'turban' | 'hijab' | 'bigHair' | 'bob' | 'straight01';
-  accessories:
-    | 'none'
-    | 'prescription01'
-    | 'prescription02'
-    | 'roundGlasses'
-    | 'sunglasses';
-  hairColor: 'brown' | 'black' | 'blonde' | 'red';
-  accessoryColor: 'black' | 'blue' | 'gray' | 'green';
+  mouth: number;
+  top: number;
+  accessories: number;
+  hairColor: number;
+  accessoryColor: number;
+  eyes: number;
+  eyebrows: number;
+  facialHair: number;
 }
 
 interface AvatarStepProps {
@@ -39,39 +87,70 @@ interface AvatarStepProps {
   nextStep: () => void;
 }
 
+// eslint-disable-next-line react/prop-types
+function CustomizationOption({ category, label, onPrevious, onNext }) {
+  return (
+    <div>
+      <div
+        className="options"
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between', // Equal space between items
+        }}
+      >
+        <Button onClick={() => onPrevious(category)}>
+          <FaArrowLeft />
+        </Button>
+        <h3 style={{ margin: '0 10px' }}>{label}</h3>
+        <Button onClick={() => onNext(category)}>
+          <FaArrowRight />
+        </Button>
+      </div>
+      <br />
+    </div>
+  );
+}
+
 export function AvatarStep({ previousStep, nextStep }: AvatarStepProps) {
   const [customization, setCustomization] = useState<CustomizationOptions>({
-    mouth: 'smile',
-    top: 'hat',
-    accessories: 'none',
-    hairColor: 'brown',
-    accessoryColor: 'black',
+    mouth: 0,
+    top: 0,
+    accessories: 0,
+    hairColor: 0,
+    accessoryColor: 0,
+    eyes: 0,
+    eyebrows: 0,
+    facialHair: 0,
   });
 
-  const handleCustomizationChange = (
-    category: keyof CustomizationOptions,
-    value: string,
-  ) => {
-    setCustomization({ ...customization, [category]: value });
+  const handleNextOption = (category: keyof CustomizationOptions) => {
+    const currentOptionIndex = customization[category];
+    const nextOptionIndex =
+      currentOptionIndex === options[category].length - 1
+        ? 0
+        : currentOptionIndex + 1;
+    setCustomization({ ...customization, [category]: nextOptionIndex });
+    console.log(currentOptionIndex);
+    console.log(category);
   };
 
-  const renderCustomizationOptions = (category: keyof CustomizationOptions) => {
-    return options[category].map((option) => (
-      <Button
-        key={option}
-        className={customization[category] === option ? 'selected' : ''}
-        onClick={() => handleCustomizationChange(category, option)}
-      >
-        {option}
-      </Button>
-    ));
+  const handleGoToPreviousOption = (category: keyof CustomizationOptions) => {
+    const currentOptionIndex = customization[category];
+    const previousOptionIndex =
+      currentOptionIndex === 0
+        ? options[category].length - 1
+        : currentOptionIndex - 1;
+    setCustomization({ ...customization, [category]: previousOptionIndex });
   };
 
   const t = useTranslations('site.onboarding-dialog.avatar-step');
   const avatar = createAvatar(avataaars, {
-    seed: 'gosholosho',
     ...Object.fromEntries(
-      Object.entries(customization).map(([key, value]) => [key, [value]]),
+      Object.entries(customization).map(([key, value]) => [
+        key,
+        [options[key][value]],
+      ]),
     ),
   });
 
@@ -79,27 +158,65 @@ export function AvatarStep({ previousStep, nextStep }: AvatarStepProps) {
     <>
       <div className="character-customization">
         <h2>Character Customization</h2>
-        <div className="options">
-          <h3>Mouth</h3>
-          {renderCustomizationOptions('mouth')}
-        </div>
-        <div className="options">
-          <h3>Top</h3>
-          {renderCustomizationOptions('top')}
-        </div>
-        <div className="options">
-          <h3>Accessories</h3>
-          {renderCustomizationOptions('accessories')}
-        </div>
-        <div className="options">
-          <h3>Hair Color</h3>
-          {renderCustomizationOptions('hairColor')}
-        </div>
-        <div className="options">
-          <h3>Accessory Color</h3>
-          {renderCustomizationOptions('accessoryColor')}
-        </div>
-        <div className="avatar-preview">
+        <CustomizationOption
+          category="eyebrows"
+          label="Eyebrows"
+          onPrevious={handleGoToPreviousOption}
+          onNext={handleNextOption}
+        />
+        <CustomizationOption
+          category="accessories"
+          label="Accessories person"
+          onPrevious={handleGoToPreviousOption}
+          onNext={handleNextOption}
+        />
+        <CustomizationOption
+          category="eyes"
+          label="Eyes"
+          onPrevious={handleGoToPreviousOption}
+          onNext={handleNextOption}
+        />
+        <CustomizationOption
+          category="accessoryColor"
+          label="Accessory Color"
+          onPrevious={handleGoToPreviousOption}
+          onNext={handleNextOption}
+        />
+        <CustomizationOption
+          category="hairColor"
+          label="Hair Color"
+          onPrevious={handleGoToPreviousOption}
+          onNext={handleNextOption}
+        />
+        <CustomizationOption
+          category="mouth"
+          label="Mouth"
+          onPrevious={handleGoToPreviousOption}
+          onNext={handleNextOption}
+        />
+        <CustomizationOption
+          category="top"
+          label="Top"
+          onPrevious={handleGoToPreviousOption}
+          onNext={handleNextOption}
+        />
+        <CustomizationOption
+          category="facialHair"
+          label="Facial Hair"
+          onPrevious={handleGoToPreviousOption}
+          onNext={handleNextOption}
+        />
+
+        <div
+          className="avatar-preview"
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            position: 'relative', // Ensure parent has position set
+            zIndex: 0, // Set lower z-index for the avatar-preview
+          }}
+        >
           <img src={avatar.toDataUriSync()} alt="" width="100" height="100" />
         </div>
       </div>
