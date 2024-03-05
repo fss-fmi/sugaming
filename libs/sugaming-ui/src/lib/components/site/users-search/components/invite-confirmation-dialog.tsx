22f58e695f105d8f06682d240db6c0f1e481db6b
Angel Penchev <angel.penchev.tsb@gmail.com>
2024-03-05 22:01:24 +0200

    refactor: user invites confirmation to use the user object + localization
    Changed the function `openInviteConfirmation` to accept a User object instead of just a user ID.
    This enables the display correct information in the dialog, such as the user's name and avatar.
    Also included are additions to translation files for text displayed in the dialog.

--- libs/sugaming-ui/src/lib/components/site/users-search/components/invite-confirmation-dialog.tsx
+++ libs/sugaming-ui/src/lib/components/site/users-search/components/invite-confirmation-dialog.tsx
@@ -2,9 +2,12 @@
 
 import React, { useState } from 'react';
 import { getBearerToken } from '@sugaming/sugaming-api-client/next';
+import { useTranslations } from 'next-intl';
+import { ApiClient } from '@sugaming/sugaming-api-client/client';
 import { Button } from '../../../common/server';
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
   DialogFooter,
@@ -15,23 +18,23 @@ import {
 } from '../../../common/client';
 
 interface InviteButtonProps {
-  userId: string;
+  user: ApiClient.UserResponseBodyDto;
   teamId: string;
   completeSuccessfulInviteConfirmation: () => void;
 }
 
 export function InviteConfirmationDialog({
-  userId,
+  user,
   teamId,
   completeSuccessfulInviteConfirmation,
 }: InviteButtonProps) {
   const { toast } = useToast();
-  // const t = useTranslations('');
+  const t = useTranslations('site.users-search.invite-confirmation-dialog');
   const [isOpen, setIsOpen] = useState(true);
 
   async function inviteUser() {
     const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_BASE}/api/v1/users/${userId}/cs2-team-invites`,
+      `${process.env.NEXT_PUBLIC_API_BASE}/api/v1/users/${user.id}/cs2-team-invites`,
       {
         method: 'POST',
         headers: {
@@ -48,7 +51,7 @@ export function InviteConfirmationDialog({
     if (response.ok) {
       toast({
         variant: 'default',
-        title: 'User invited successfully',
+        title: t('invite-successful'),
       });
       completeSuccessfulInviteConfirmation();
       return;
@@ -58,8 +61,8 @@ export function InviteConfirmationDialog({
     const json = await response.json();
     toast({
       variant: 'destructive',
-      title: json.message || "t('error-occurred')",
-      description: "t('try-again')",
+      title: json.message || t('error-occurred'),
+      description: t('try-again'),
     });
   }
 
@@ -70,14 +73,14 @@ export function InviteConfirmationDialog({
       <Dialog open={isOpen} onOpenChange={setIsOpen}>
         <DialogContent className="sm:max-w-[425px]">
           <DialogHeader>
-            <DialogTitle>Invite user</DialogTitle>
+            <DialogTitle>{t('title')}</DialogTitle>
             <DialogDescription>
-              Make changes to your profile here. Click save when
+              {t('description', { name: `${user.firstName} ${user.lastName}` })}
             </DialogDescription>
           </DialogHeader>
-          <div className="grid gap-4 py-4">gosho</div>
           <DialogFooter>
-            <Button onClick={() => inviteUser()}>Save changes</Button>
+            <DialogClose>{t('cancel')}</DialogClose>
+            <Button onClick={() => inviteUser()}>{t('invite')}</Button>
           </DialogFooter>
         </DialogContent>
       </Dialog>
