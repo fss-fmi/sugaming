6a7a55c8c19cc0311cefa2ac57e1eb114c81da25
Angel Penchev <angel.penchev.tsb@gmail.com>
2024-04-19 14:53:15 +0300

    fix: invert navbar logo on light mode
    Inverted the navbar logo in light mode, so it is visible in light mode.

--- libs/sugaming-ui/src/lib/components/site/logo/logo.tsx
+++ libs/sugaming-ui/src/lib/components/site/logo/logo.tsx
@@ -1,20 +1,26 @@
+'use client';
+
 import Image from 'next/image';
 import React from 'react';
+import { useTheme } from 'next-themes';
 import { cn } from '../../../utils';
 
 export function Logo(props: React.HTMLAttributes<HTMLDivElement>) {
   // eslint-disable-next-line react/prop-types
   const { className, ...otherProps } = props;
+  const { resolvedTheme } = useTheme();
+
   return (
     <div
       className={cn('flex w-fit items-center gap-1.5', className)}
       {...otherProps}
     >
       <Image
-        src="/assets/icons/sugaming.png"
+        src="/assets/icons/sugaming-light.png"
         alt="SUGAMING Logo"
         width={32}
         height={32}
+        className={resolvedTheme === 'light' ? 'filter invert' : ''}
       />
       <span className="text-xl font-bold">SUGAMING</span>
     </div>
