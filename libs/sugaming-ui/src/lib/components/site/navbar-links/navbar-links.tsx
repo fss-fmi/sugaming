1a72c6237271a2daabf3e4ee44809961a17f9431
Angel Penchev <angel.penchev.tsb@gmail.com>
2024-04-21 21:27:00 +0300

    fix(#134): Remove redundant links + Favicon and title fix (#135)
    * feat: temporary remove redundant links
    
    * feat: change favicon and page title

--- libs/sugaming-ui/src/lib/components/site/navbar-links/navbar-links.tsx
+++ libs/sugaming-ui/src/lib/components/site/navbar-links/navbar-links.tsx
@@ -1,4 +1,3 @@
-import { useLocale, useTranslations } from 'next-intl';
 import { NavbarLinksDesktop } from './components/navbar-links-desktop';
 import { NavbarLinksMobile } from './components/navbar-links-mobile';
 
@@ -7,80 +6,7 @@ interface NavbarLinksProps {
   className: string;
 }
 export function NavbarLinks({ className, variant }: NavbarLinksProps) {
-  const locale = useLocale();
-  const t = useTranslations('site.navbar-links');
-
-  const links = [
-    {
-      title: t('league.name'),
-      href: `/${locale}/league`,
-      links: [
-        {
-          isFeatured: true,
-          title: t('league.name'),
-          description: t('league.description'),
-          href: `/${locale}/league`,
-        },
-        {
-          isFeatured: false,
-          title: t('league.tournaments.name'),
-          description: t('league.tournaments.description'),
-          href: `/${locale}/league/tournaments`,
-        },
-        {
-          isFeatured: false,
-          title: t('league.matches.name'),
-          description: t('league.matches.description'),
-          href: `/${locale}/league/matches`,
-        },
-        {
-          isFeatured: false,
-          title: t('league.teams.name'),
-          description: t('league.teams.description'),
-          href: `/${locale}/league/teams`,
-        },
-      ],
-    },
-    {
-      title: t('cs2.name'),
-      href: `/${locale}/cs2`,
-      links: [
-        {
-          isFeatured: true,
-          title: t('cs2.name'),
-          description: t('cs2.description'),
-          href: `/${locale}/cs2`,
-        },
-        {
-          isFeatured: false,
-          title: t('cs2.tournaments.name'),
-          description: t('cs2.tournaments.description'),
-          href: `/${locale}/cs2/tournaments`,
-        },
-        {
-          isFeatured: false,
-          title: t('cs2.matches.name'),
-          description: t('cs2.matches.description'),
-          href: `/${locale}/cs2/matches`,
-        },
-        {
-          isFeatured: false,
-          title: t('cs2.teams.name'),
-          description: t('cs2.teams.description'),
-          href: `/${locale}/cs2/teams`,
-        },
-      ],
-    },
-    {
-      title: t('valorant.name'),
-      href: `/${locale}/valorant`,
-      badge: t('valorant.coming-soon'),
-    },
-    {
-      title: t('game-dev'),
-      href: `/${locale}/game-dev`,
-    },
-  ];
+  const links = [];
 
   if (variant === 'desktop') {
     return <NavbarLinksDesktop className={className} links={links} />;
