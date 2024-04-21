c0de63dcb2b7f3acc49c24e98076dd555ffe9cc1
Angel Penchev <angel.penchev.tsb@gmail.com>
2024-04-21 19:45:01 +0300

    feat: add hero action button and update i18n files
    A new HeroActionButton component was added to allow users to join Discord or sign up.
    Translations for these actions were also added to the i18n files in both English and Bulgarian.
    The button will render differently based on whether a user is logged in and if they have already
    joined the Discord server.

---
+++ libs/sugaming-ui/src/lib/components/site/hero-action-button/hero-action-button.tsx
@@ -0,0 +1,47 @@
+import { getUser } from '@sugaming/sugaming-api-client/next';
+import Link from 'next/link';
+import React from 'react';
+import { useLocale } from 'next-intl';
+import { getTranslations } from 'next-intl/server';
+import { FaDiscord } from 'react-icons/fa6';
+import { Button } from '../../common/server';
+
+export async function HeroActionButton() {
+  const t = await getTranslations('site.hero-action-button');
+  const locale = useLocale();
+  const user = await getUser();
+
+  if (!user) {
+    return (
+      <Button asChild>
+        <Link href={`/${locale}/signup`}>{t('join-us')}</Link>
+      </Button>
+    );
+  }
+
+  if (user && !user.discord) {
+    return (
+      <Button asChild>
+        <Link
+          href={`${process.env.NEXT_PUBLIC_API_BASE}/api/v1/auth/login/discord`}
+          target="_blank"
+          rel="opener" // Required, so that the new tab can go back to this window
+        >
+          <FaDiscord className="w-6 h-6 mr-2" />
+          {t('join-us-on-discord')}
+        </Link>
+      </Button>
+    );
+  }
+
+  return (
+    <Button asChild>
+      <Link
+        href={`https://discord.com/channels/${process.env.DISCORD_GUILD_ID}`}
+      >
+        <FaDiscord className="w-6 h-6 mr-2" />
+        {t('go-to-our-discord')}
+      </Link>
+    </Button>
+  );
+}
