60ab0ac8e5f1707617624bbe4535a89f133dd844
Angel Penchev <angel.penchev.tsb@gmail.com>
2024-04-26 18:26:13 +0000

    feat(#118): add regulations page and related components (#144)
    * feat: add regulations page and related components
    A new Regulations page has been added, along with a corresponding component in the UI library.
    i18n support has been provided with separate JSON files for English and Bulgarian languages.
    The RegulationItem component enables viewing of specific regulations within the Regulations page.

--- libs/sugaming-ui/src/lib/components/site/client.ts
+++ libs/sugaming-ui/src/lib/components/site/client.ts
@@ -7,6 +7,7 @@ export * from './navbar-links/navbar-links';
 export * from './navbar-user-controls/navbar-user-controls';
 export * from './notifications-popover/notifications-popover';
 export * from './onboarding-dialog/onboarding-dialog';
+export * from './regulation-item/regulation-item';
 export * from './sign-up-form/sign-up-form';
 export * from './team-member-avatar/team-member-avatar';
 export * from './users-search/users-search';
